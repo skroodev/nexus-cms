6fe01fd120a163254f18050f5f7b02139e2a4ab5
BAEK <t.becq.contact@gmail.com>
2026-02-03 17:28:18 +0100

    fix db

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,11 +1,8 @@
-import fs from 'fs'
 import path from 'path'
-import { sqliteD1Adapter } from '@payloadcms/db-d1-sqlite'
+import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
-import { CloudflareContext, getCloudflareContext } from '@opennextjs/cloudflare'
-import { GetPlatformProxyOptions } from 'wrangler'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -13,14 +10,6 @@ import { Posts } from './collections/Posts'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
-const realpath = (value: string) => (fs.existsSync(value) ? fs.realpathSync(value) : undefined)
-
-const isCLI = process.argv.some((value) => realpath(value).endsWith(path.join('payload', 'bin.js')))
-const isProduction = process.env.NODE_ENV === 'production'
-
-const cloudflare = isProduction
-  ? await getCloudflareContext({ async: true })
-  : await getCloudflareContextFromWrangler()
 
 export default buildConfig({
   admin: {
@@ -63,22 +52,11 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: sqliteD1Adapter({
-    binding: isProduction ? cloudflare.env.D1 : undefined,
+  db: sqliteAdapter({
+    url: process.env.DATABASE_URL || `file:${path.resolve(dirname, 'payload.db')}`,
   }),
   plugins: [
     // For local development, images are stored in memory
     // In production on Vercel, Vercel Blob is used automatically for Media collection
   ],
 })
-
-// Adapted from https://github.com/opennextjs/opennextjs-cloudflare/blob/d00b3a13e42e65aad76fba41774815726422cc39/packages/cloudflare/src/api/cloudflare-context.ts#L328C36-L328C46
-function getCloudflareContextFromWrangler(): Promise<CloudflareContext> {
-  return import(/* webpackIgnore: true */ `${'__wrangler'.replaceAll('_', '')}`).then(
-    ({ getPlatformProxy }) =>
-      getPlatformProxy({
-        environment: process.env.CLOUDFLARE_ENV,
-        remoteBindings: isProduction,
-      } satisfies GetPlatformProxyOptions),
-  )
-}
