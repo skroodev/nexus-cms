bec31f392618070e87db838bf6a1c56aa3cbf69d
BAEK <t.becq.contact@gmail.com>
2026-02-03 19:44:58 +0100

    feat: add posts table migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250929_111647 from './20250929_111647'
+import * as migration_20250929_111647 from './20250929_111647';
+import * as migration_20260203_183704 from './20260203_183704';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250929_111647.down,
     name: '20250929_111647',
   },
-]
+  {
+    up: migration_20260203_183704.up,
+    down: migration_20260203_183704.down,
+    name: '20260203_183704'
+  },
+];
